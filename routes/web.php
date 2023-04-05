a9bf0a67dbfa9eef75b4ca13e50b701673d538d9
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2023-04-06 00:46:56 +0330

    add route user contoller

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ $router->get('/uptime' ,'monitoringController@uptime');
 $router->get('/info' ,'monitoringController@getServerInfo');
 $router->get('/diskUsage' ,'monitoringController@diskUsage');
 $router->get('/cpuAndRam' ,'monitoringController@cpuAndRam');
+$router->get('/onlineUser' ,'monitoringController@onlineUser');
+
+$router->post('/users/create' ,'UserController@create');
