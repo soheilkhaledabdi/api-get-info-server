70ad9c9018c90de461b3a575e02f00423651604c
soheil khaledabadi <soheilkhaledabdi@gmail.com>
2023-04-06 01:53:10 +0330

    Complete user code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 
 use App\Libraries\Responder\Facades\ResponderFacade;
+use App\Libraries\Responder\ResponseBuilder;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 
@@ -11,11 +12,24 @@ class UserController extends Controller
 {
     public function create(Request $request)
     {
-        $username = $request->username; // Replace with desired username
-        $password = $request->password; // Replace with desired password
+        $Data =  $this->validate($request , [
+            'username' => 'required',
+            'password' => 'required',
+            'date' => 'required',
+            'limit' => 'required'
+        ]);
+
+        $username = $Data['username'];
+        $password = $Data['password'];
+        $date = $Data['date'];
+        $limit = $Data['limit'];
 
         // Execute the adduser command
         shell_exec("sudo useradd {$username} -p $(openssl passwd -1 {$password})");
-        return response()->json(['message' => 'User created successfully.']);
+        shell_exec("chage -E {$date} {$username}");
+        shell_exec("sudo bash -c echo '{$username} hard maxlogins {$limit}' >> /etc/security/limits.conf");
+
+        $response = new ResponseBuilder();
+        return  $response->setMessage('User created successfully')->respond();
     }
 }
