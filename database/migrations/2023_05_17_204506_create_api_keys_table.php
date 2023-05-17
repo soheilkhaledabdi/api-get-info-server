77275e42d9368b53f3e2d3e5f4de156603c533d5
soheil khaledabdi <soheilkhaledabdi@gmail.com>
2023-05-17 14:02:34 -0700

    create migration for save api key

---
+++ database/migrations/2023_05_17_204506_create_api_keys_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('api_keys', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique();
+            $table->text('description')->nullable();
+//            $table->timestamp('expire_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('api_keys');
+    }
+};
