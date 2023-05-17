85ef3527708f2dd625f90678bd38b3c4c256ce0e
soheil khaledabdi <soheilkhaledabdi@gmail.com>
2023-05-17 14:02:50 -0700

    add route apis

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@
 
 
 
+
+$router->get('/api-key' , function () {
+   return view('api');
+});
+
 $router->get('/uptime' ,'monitoringController@uptime');
 $router->get('/info' ,'monitoringController@getServerInfo');
 $router->get('/diskUsage' ,'monitoringController@diskUsage');
@@ -12,6 +17,8 @@ $router->get('/onlineUser' ,'monitoringController@onlineUser');
 $router->get('/getAllUsers' ,'monitoringController@getAllUsers');
 $router->get('/reboot' ,'monitoringController@onlineUser');
 $router->get('/update' ,'monitoringController@onlineUser');
+
+$router->post('/api-keys', 'ApiKeyController@store');
 
 
 $router->group(['prefix' => 'users'] , function () use ($router){
@@ -22,6 +29,5 @@ $router->group(['prefix' => 'users'] , function () use ($router){
     $router->post('/check' ,'UserController@check_user_exsit');
     $router->post('/active' ,'UserController@is_user_active');
     $router->get('/online' ,'UserController@online');
-
 });
 
